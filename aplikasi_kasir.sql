540a408a8a3a53b4129eddf755ee5155dfadc2a9
giehandhini10 <161487626+giehandhini10@users.noreply.github.com>
2024-03-01 07:19:09 +0700

    Add files via upload

---
+++ aplikasi_kasir.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 29, 2024 at 03:09 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aplikasi_kasir`
+--
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `total_harga_transaksi` ()   BEGIN
+SELECT 
+SUM(tb_keranjang.jumlah*tb_keranjang.harga) AS total_harga
+FROM tb_keranjang;
+END$$
+
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'rony09', '090601'),
+(2, 'salma12', '120202');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_databarang`
+--
+
+CREATE TABLE `tb_databarang` (
+  `kode_barang` int(50) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `stok` int(10) NOT NULL,
+  `tanggal` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_databarang`
+--
+
+INSERT INTO `tb_databarang` (`kode_barang`, `nama_barang`, `harga`, `stok`, `tanggal`) VALUES
+(2023, 'Serum Skintific', 99000, 50, '2024-02-29'),
+(2024, 'Toner Emina 50ml', 13000, 29, '2024-02-29'),
+(2025, 'Cusion Something', 148000, 55, '2024-02-29'),
+(2026, 'Serum Acne Implora', 22000, 15, '2024-02-29'),
+(2027, 'Moisturizer The Orizinote', 45000, 18, '2024-02-29'),
+(2028, 'Sunscreen Facetology', 75000, 45, '2024-02-29'),
+(2029, 'Lipcream Hanasui', 25000, 32, '2024-02-29'),
+(2030, 'Liptint Luxcrime', 99000, 9, '2024-02-29'),
+(2031, 'Seting Spray Sea Makeup', 69000, 44, '2024-02-29'),
+(2032, 'Seting Spray Luxcrime Glow 50ml', 67000, 23, '2024-02-29'),
+(2033, 'Seting Spray Luxcrime Matte', 67000, 30, '2024-02-29'),
+(2034, 'Face Wash Scarlet', 75000, 33, '2024-02-29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_datapetugas`
+--
+
+CREATE TABLE `tb_datapetugas` (
+  `id_petugas` int(11) NOT NULL,
+  `nama_petugas` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `alamat` text NOT NULL,
+  `tanggal_pendaftaran` date NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_datapetugas`
+--
+
+INSERT INTO `tb_datapetugas` (`id_petugas`, `nama_petugas`, `email`, `alamat`, `tanggal_pendaftaran`, `username`, `password`) VALUES
+(12, 'Nabila', 'Nabila21@gmail.com', 'Jakarta', '2024-02-12', 'nabila21', '211105');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_keranjang`
+--
+
+CREATE TABLE `tb_keranjang` (
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` int(10) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `jumlah` int(10) NOT NULL,
+  `total_harga` int(10) NOT NULL,
+  `tgl_transaksi` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_keranjang`
+--
+
+INSERT INTO `tb_keranjang` (`id_transaksi`, `kode_barang`, `nama_barang`, `harga`, `jumlah`, `total_harga`, `tgl_transaksi`) VALUES
+(29, 2031, 'Seting Spray Sea Makeup', 69000, 1, 69000, '2024-02-29'),
+(30, 2034, 'Face Wash Scarlet', 75000, 2, 150000, '2024-02-29');
+
+--
+-- Triggers `tb_keranjang`
+--
+DELIMITER $$
+CREATE TRIGGER `cancel` AFTER DELETE ON `tb_keranjang` FOR EACH ROW BEGIN
+UPDATE tb_databarang SET
+stok = stok + OLD.jumlah
+WHERE kode_barang = OLD.kode_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `cancel_2` AFTER DELETE ON `tb_keranjang` FOR EACH ROW BEGIN
+DELETE FROM transaksi
+WHERE kode_barang = OLD.kode_barang;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `stok_habis` AFTER INSERT ON `tb_keranjang` FOR EACH ROW BEGIN
+DELETE FROM tb_databarang
+WHERE stok = 0
+AND
+kode_barang = NEW.kode_barang;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `tgl_transaksi` date NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `kode_barang` int(50) NOT NULL,
+  `nama_barang` varchar(50) NOT NULL,
+  `harga` int(11) NOT NULL,
+  `jumlah_barang` int(10) NOT NULL,
+  `total_harga` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`tgl_transaksi`, `id_transaksi`, `kode_barang`, `nama_barang`, `harga`, `jumlah_barang`, `total_harga`) VALUES
+('2024-02-29', 26, 2027, 'Moisturizer The Orizinote', 45000, 2, 90000),
+('2024-02-29', 27, 2024, 'Toner Emina 50ml', 13000, 1, 13000),
+('2024-02-29', 28, 2030, 'Liptint Luxcrime', 99000, 3, 297000),
+('2024-02-29', 29, 2031, 'Seting Spray Sea Makeup', 69000, 1, 69000),
+('2024-02-29', 30, 2034, 'Face Wash Scarlet', 75000, 2, 150000);
+
+--
+-- Triggers `transaksi`
+--
+DELIMITER $$
+CREATE TRIGGER `keranjang` AFTER INSERT ON `transaksi` FOR EACH ROW BEGIN
+INSERT INTO tb_keranjang SET
+id_transaksi = NEW.id_transaksi,
+kode_barang = NEW.kode_barang,
+nama_barang = NEW.nama_barang,
+harga = NEW.harga,
+jumlah = NEW.jumlah_barang,
+total_harga = NEW.total_harga,
+tgl_transaksi = NEW.tgl_transaksi;
+END
+$$
+DELIMITER ;
+DELIMITER $$
+CREATE TRIGGER `transaksi` AFTER INSERT ON `transaksi` FOR EACH ROW BEGIN
+UPDATE tb_databarang SET
+stok = stok - NEW.jumlah_barang
+WHERE kode_barang = NEW.kode_barang;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_databarang`
+--
+ALTER TABLE `tb_databarang`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indexes for table `tb_datapetugas`
+--
+ALTER TABLE `tb_datapetugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id_transaksi`),
+  ADD KEY `kode_barang` (`kode_barang`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tb_databarang`
+--
+ALTER TABLE `tb_databarang`
+  MODIFY `kode_barang` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2035;
+
+--
+-- AUTO_INCREMENT for table `tb_datapetugas`
+--
+ALTER TABLE `tb_datapetugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `tb_keranjang`
+--
+ALTER TABLE `tb_keranjang`
+  ADD CONSTRAINT `tb_keranjang_ibfk_1` FOREIGN KEY (`kode_barang`) REFERENCES `tb_databarang` (`kode_barang`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
